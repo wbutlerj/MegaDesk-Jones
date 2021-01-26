3c57d535cd8836360d36d661fc354de93f035c61
Weston Jones <Weston Jones@SPACESTATION>
2021-01-26 14:59:16 -0700

    Add project files.

---
+++ MegaDesk-Jones/AddQuote.Designer.cs
@@ -0,0 +1,71 @@
+﻿
+namespace MegaDesk_Jones
+{
+    partial class AddQuote
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(290, 74);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(265, 29);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(124, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Customer Name:";
+            // 
+            // AddQuote
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(881, 506);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "AddQuote";
+            this.Text = "Add New Quote";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ MegaDesk-Jones/MainMenu.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace MegaDesk_Jones
+{
+    partial class MainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAddQuote = new System.Windows.Forms.Button();
+            this.btnViewQuotes = new System.Windows.Forms.Button();
+            this.btnSearchQuotes = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAddQuote
+            // 
+            this.btnAddQuote.Location = new System.Drawing.Point(84, 57);
+            this.btnAddQuote.Name = "btnAddQuote";
+            this.btnAddQuote.Size = new System.Drawing.Size(254, 75);
+            this.btnAddQuote.TabIndex = 0;
+            this.btnAddQuote.Text = "Add New Quote";
+            this.btnAddQuote.UseVisualStyleBackColor = true;
+            // 
+            // btnViewQuotes
+            // 
+            this.btnViewQuotes.Location = new System.Drawing.Point(84, 159);
+            this.btnViewQuotes.Name = "btnViewQuotes";
+            this.btnViewQuotes.Size = new System.Drawing.Size(254, 75);
+            this.btnViewQuotes.TabIndex = 1;
+            this.btnViewQuotes.Text = "View Quotes";
+            this.btnViewQuotes.UseVisualStyleBackColor = true;
+            // 
+            // btnSearchQuotes
+            // 
+            this.btnSearchQuotes.Location = new System.Drawing.Point(84, 258);
+            this.btnSearchQuotes.Name = "btnSearchQuotes";
+            this.btnSearchQuotes.Size = new System.Drawing.Size(254, 75);
+            this.btnSearchQuotes.TabIndex = 2;
+            this.btnSearchQuotes.Text = "Search Quotes";
+            this.btnSearchQuotes.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(84, 359);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(254, 75);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MegaDesk_Jones.Properties.Resources.teacher_desk;
+            this.pictureBox1.Location = new System.Drawing.Point(485, 129);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(256, 258);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(925, 544);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnSearchQuotes);
+            this.Controls.Add(this.btnViewQuotes);
+            this.Controls.Add(this.btnAddQuote);
+            this.Name = "MainMenu";
+            this.Text = "Main Menu";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAddQuote;
+        private System.Windows.Forms.Button btnViewQuotes;
+        private System.Windows.Forms.Button btnSearchQuotes;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
